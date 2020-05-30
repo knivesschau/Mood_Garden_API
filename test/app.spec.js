ccda64948c414226cad9c20d12c0e2cb702f8d04
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-05-30 00:44:53 -0700

    tweaking init deploy

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,7 +3,7 @@ const app = require('../src/app')
 describe('App', () => {
   it('GET /api responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
-      .get('/api/*')
+      .get('/')
       .expect(200, 'Hello, world!')
   })
 })
