2eec6036c7c1b7cd0a6b7103919fd3175b9ba2f9
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-06-03 01:15:52 -0700

    created first table and seeded mock data

---
+++ migrations/002.do.create_garden_users.sql
@@ -0,0 +1,3 @@
+CREATE TABLE garden_users (
+    
+);
---
+++ seeds/seed.rose_entries.sql
@@ -0,0 +1,38 @@
+INSERT INTO rose_entries(rose, thorn, bud, color)
+    VALUES
+     (
+        'I got to video chat with my family and eat dinner together virtually. We made eggplant parmesean together!',
+        'Even though I saw my family via video chat, I realized how much I missed them today and it mad me sad that I haven"t been able to see them for a long time.',
+        'I"m looking forward to seeing my family again once shelter-in-place is over.',
+         'Yellow'
+     ),
+     (
+         'I learned a new roller skating trick!',
+         'Even though I learned a new trick, I was feeling frustrated that I kept falling on my skates today. It makes me feel like I"m not improving as much as I think.',
+         'I want to continue working on getting better at roller skating and learn more tricks.',
+         'Red'
+     ),
+     (
+          'I was able to get a lot of homework done today and get some excercise.',
+          'I got stressed out thinking about how much studying I have to do before finals begin.',
+          'I am looking forward to graduating from school in one month and having more time to relax and see friends.',
+          'White'
+     ),
+     (
+         'I got some much needed rest today and was able to sleep in until noon. I was also happy that I got the clothes I ordered in the mail.',
+         'It made me a little upset that my ideas for a surprise party were ignored during a conversation with my friends.',
+         'This weekend, I want to learn how to make sourdough bread from scratch.',
+         'Pink'
+     ),
+     (
+         'I was able to respond to all of my e-mails, complete all my daily tasks, and clean out my (extremely messy!) inbox.',
+         'I had an extremely upsetting conversation with a work colleague today- it made me feel invalidated and unappreciated.',
+         'I"m looking forward to painting my nails tomorrow after work and trying out some new nail art designs.',
+         'Purple'
+     ),
+     (
+         'I saw a puppy on my walk and was able to pet him- he was so soft!',
+         'Lately, I"ve been feeling like my self-worth has been down the drain. I can"t stop comparing my success to others and I constantly feel like everyone is further ahead in life than I am.',
+         'I"m looking forward to writing a new song with my bandmates',
+         'Black'
+     );
